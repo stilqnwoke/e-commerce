08032fcafbbf4abe3e50027146c4b5595dda3b73
Stilqn G <debelakoto@gmail.com>
2021-03-15 16:32:49 +0200

    Checkout product setup

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useStateValue } from './StateProvider';
+import CheckoutProduct from "./CheckoutProduct";
 import "./Checkout.css";
 
 function Checkout() {
@@ -8,9 +9,33 @@ function Checkout() {
     return <div className="checkout">
         <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" />
 
+        {basket?.length === 0} ? (
+        <div>
+            <h2>
+                Your shopping basket is empty.
+            </h2>
+        </div>
+        ) : (
+          <div>
+            <h2 className="checkout__title">Your Shopping Basket</h2>
 
+
+            {/* List out all of the Checkout products*/}
+            {basket.map(item => (
+                <CheckoutProduct
+                    item = {item.id}
+                    title = {item.title}
+                    image = {item.image}
+                    price = {item.price}
+                    rating = {item.rating}
+                />
+            ))}
+
+          </div>
+        )}
     </div>
-    
+
+
 }
 
 export default Checkout

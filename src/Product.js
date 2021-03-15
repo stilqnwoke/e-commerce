0715b2bf7304bb80d66a949657ba73c1c563424a
Stilqn G <debelakoto@gmail.com>
2021-03-15 15:47:52 +0200

    Add to cart feature is done

--- src/Product.js
+++ src/Product.js
@@ -7,7 +7,16 @@ function Product({ id, title, image, price, rating }) {
 
     const addToBasket = () => {
         // Add item to basket
-
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id, id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
     };
 
 

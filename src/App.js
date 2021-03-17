3fdd3da9f56538b08afcf4611de4a3bb8d0de9f0
Stilqn G <debelakoto@gmail.com>
2021-03-17 11:52:50 +0200

    added Stripe dependencies for payment processing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import Login from "./Login";
 import Payment from "./Payment";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51IVvrmEOlirF5L40TuXssaHeFoutcGqNqEsarqPa6SilsbMJs8KR0AQUUN1nFbLJmHlFVewJzXFSSMdNb6Ae3EIz00YYAerqrx");
 
 
 function App() {
@@ -17,52 +22,54 @@ function App() {
   // code which runs based on a condition
 
   useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged((authUser) => {
-        if (authUser) {
-            // the user is logged in
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user is logged in
 
-            dispatch ({
-              type: "SET_USER",
-              user: authUser
-            });
-        } else {
-            // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: authUser
+        });
+      } else {
+        // the user is logged out
 
-            dispatch ({
-              type: "SET_USER",
-              user: null,
-            });
-        }
-      });
-
-      return () => {
-        // cleanup operations here
-
-        unsubscribe();
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
       }
+    });
+
+    return () => {
+      // cleanup operations here
+
+      unsubscribe();
+    }
 
   }, [])
 
   return (
     <Router>
       <div className="app">
-          <Switch>
-              <Route path="/checkout">
-                  <Header />
-                  <Checkout />
-              </Route>
-              <Route path="/login">
-                  <Login />
-              </Route>
-              <Route path="/payment">
-                  <Header />
-                  <Payment />
-              </Route>
-              <Route path="/">
-                  <Header />
-                  <Home />
-              </Route>
-          </Switch>
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );

4ac26fe0b1f83c58634937b637ec08dae6ca671e
Stilqn G <debelakoto@gmail.com>
2021-03-24 01:40:20 +0200

    Footer added

--- src/Footer.js
+++ src/Footer.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import "./Footer.css";
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 function Footer() {
     return (
         <div className="footer">
             <article className="services">
                 <ul>
-                    <li>Terms of Service</li>
-                    <li>Contacts</li>
-                    <li>Deals</li>
+                    <li><a>Terms of Service</a></li>
+                    <li><a>Contacts</a></li>
+                    <li><a>Deals</a></li>
 
                 </ul>
                 <ul>
-                    <li>Shipping</li>
-                    <li>Store Locator</li>
-                    <li>Investor Relations</li>
+                    <li><a>Shipping</a></li>
+                    <li><a>Store Locator</a></li>
+                    <li><a>Investor Relations</a></li>
                 </ul>
-                <ul>
-                    <li>Shipping</li>
-                    <li>Store Locator</li>
-                    <li>Investor Relations</li>
+                <ul className="social__icons">
+                    <li><InstagramIcon /></li>
+                    <li><YouTubeIcon /></li>
+                    <li><FacebookIcon /></li>
                 </ul>
             </article>
         </div>
